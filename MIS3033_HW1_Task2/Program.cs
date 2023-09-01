72a369c490f9b6aa181729c57dc11b8862cb7795
alp08 <alp08@BOOK-QJR6576NGV>
2023-09-01 11:18:55 -0500

    Add project files.

---
+++ MIS3033_HW1_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// MIS 3033
+// Task 2
+
+double totalgrade = 0.0;
+string mesg = "";
+int numofexams = 3;
+for (int i = 0; i <numofexams; i++)
+{
+    mesg = string.Format("Please input exam {0}:", i + 1);
+    Console.Write(mesg);
+    string grade = Console.ReadLine();
+    double examgrade = Convert.ToDouble(grade);
+    totalgrade = totalgrade + examgrade;
+}
+
+double avrggrade = totalgrade / numofexams;
+double avrggradepercent = avrggrade / 100;
+mesg = String.Format("The final grade percentage is {0:P2}",avrggradepercent);
+Console.WriteLine(mesg);
